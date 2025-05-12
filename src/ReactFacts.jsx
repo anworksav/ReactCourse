138da723cb2054dd3a1181b8d7f965419f82a161
anworksav <anworksav@gmail.com>
2025-05-12 11:30:12 +0300

    Custom component p2

---
+++ src/ReactFacts.jsx
@@ -0,0 +1,22 @@
+function ReactFacts() {
+    return (
+        <div>
+            <header>
+                <img src="src/assets/react.svg" width="40px" alt="logo.svg"/>
+            </header>
+            <main>
+                <h1>Reasons I'm excited to learn React</h1>
+                <ol>
+                    <li>Reason 1</li>
+                    <li>Reason 2</li>
+                    <li>Reason 3</li>
+                </ol>
+            </main>
+            <footer>
+                <small>"2025 Anastasia development. All rights reserved."</small>
+            </footer>
+        </div>
+    )
+}
+
+export default ReactFacts
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client"
+import ReactFacts from './ReactFacts.jsx'
+
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <ReactFacts />
+)
