6b9d4950baecdb4de7a3d79f0b68391f1f370c74
anworksav <anworksav@gmail.com>
2025-05-14 15:03:46 +0300

    TravelJournalPt1PropVariant2

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,7 @@ export default function App() {
     const arr = data.map((element) => {
         return <Entry 
                     key={element.id}
-                    entry={element}
-                    // img={{
-                    //     src:element.img.src,
-                    //     alt:element.img.alt
-                    // }}
-                    // title={element.title}
-                    // country={element.country}
-                    // googleMapsLink={element.googleMapsLink}
-                    // dates={element.dates}
-                    // text={element.text}
+                    {...element}
                 />
     })
     console.log(arr)
--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,17 +1,16 @@
 import './index.css'
 
 export default function Entry(props) {
-    console.log(props)
     return (
         <article className='article'>
-            <img className='img-photo' src={props.entry.img.src} alt={props.entry.img.alt}/>
+            <img className='img-photo' src={props.img.src} alt={props.img.alt}/>
             <div className='article-info'>
                 <img className='img-location' src='src/assets/location.png' alt='location'/>
-                <span className='span-location'>{props.entry.country}</span>
-                <a className='a-location' href={props.entry.googleMapsLink}>View on Google Maps</a>
-                <h1 className='article-name'>{props.entry.title}</h1>
-                <h2 className='article-dates'>{props.entry.dates}</h2>
-                <p className='article-text'>{props.entry.text}</p>
+                <span className='span-location'>{props.country}</span>
+                <a className='a-location' href={props.googleMapsLink}>View on Google Maps</a>
+                <h1 className='article-name'>{props.title}</h1>
+                <h2 className='article-dates'>{props.dates}</h2>
+                <p className='article-text'>{props.text}</p>
             </div>
         </article>
     )
